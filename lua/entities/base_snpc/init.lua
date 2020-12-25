3ed508ca028b98d872030bd226e84299443337c9
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-12-25 04:52:47 +0300

    Update init.lua

--- lua/entities/base_snpc/init.lua
+++ lua/entities/base_snpc/init.lua
@@ -6,15 +6,19 @@ ENT.Sequence = "idle01"
 ENT.Model = "models/Barney.mdl"
 
 function ENT:Initialize()
-  self:SetModel(model)
+  	self:SetModel(model)
 	self:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
 	self:SetUseType(SIMPLE_USE)
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_NONE)
 	self:SetSolid(SOLID_BBOX)
 
+	self:RunAnimation(self.Sequence)
+end
+
+function ENT:RunAnimation(anim)
 	self:ResetSequence(
-		self:LookupSequence(self.Sequence)
+		self:LookupSequence(anim)
 	)
 	self:SetCycle(0)
 	self:SetPlaybackRate(1)
