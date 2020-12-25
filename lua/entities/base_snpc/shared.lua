6988115937610589817df45f42b18e034db4c429
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-12-25 04:54:24 +0300

    Update shared.lua

--- lua/entities/base_snpc/shared.lua
+++ lua/entities/base_snpc/shared.lua
@@ -1,6 +1,12 @@
 ENT.Base = "base_entity"
 ENT.AutomaticFrameAdvance = true
 
+ENT.Spawnable 	= false -- its base :v
+ENT.AdminOnly 	= false
+
+ENT.Author       = "Beelzebub"
+ENT.Contact      = "beelzebub@incredible-gmod.ru"
+
 function ENT:SetAutomaticFrameAdvance(bool)
 	self.AutomaticFrameAdvance = bool
 end
