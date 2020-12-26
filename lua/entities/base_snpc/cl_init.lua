3d872197cbfa5122f840bc1ff8c8c98a97a3a598
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-12-26 21:24:32 +0300

    Update cl_init.lua

--- lua/entities/base_snpc/cl_init.lua
+++ lua/entities/base_snpc/cl_init.lua
@@ -5,3 +5,11 @@ include("shared.lua")
 function ENT:Draw(f)
 	self:DrawModel(f)
 end
+
+net.Receive(ENT.NetID, function()
+	local ent = net.ReadEntity()
+
+	if ent.OnUse then
+		ent:OnUse()
+	end
+end)
