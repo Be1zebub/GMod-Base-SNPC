a929826f66a5a53339f664117ef630f8c7987e9d
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-12-26 21:23:51 +0300

    Update example_snpc.lua

--- example_snpc.lua
+++ example_snpc.lua
@@ -1,3 +1,5 @@
+AddCSLuaFile()
+
 ENT.Base = "base_snpc"
 
 ENT.PrintName = "My 1st SNPC"
@@ -6,3 +8,21 @@ ENT.Category = "My Stuff"
 
 ENT.Spawnable 	= true
 ENT.AdminOnly 	= true
+
+ENT.Model = "models/characters/hostage_04.mdl"
+ENT.Sequence = "LineIdle04"
+
+if CLIENT then
+	function ENT:OnUse()
+		local menu = vgui.Create("DFrame")
+		menu:SetSize(400, 600)
+		menu:Center()
+		menu:MakePopup()
+		menu:SetTitle(self:GetClass())
+
+		menu.lbl = menu:Add("DLabel")
+		menu.lbl:SetText("Hello world!")
+		menu.lbl:SizeToContents()
+		menu.lbl:SetPos(menu:GetWide() * 0.5 - menu.lbl:GetWide() * 0.5, menu:GetTall() * 0.5 - menu.lbl:GetTall() * 0.5)
+	end
+end
