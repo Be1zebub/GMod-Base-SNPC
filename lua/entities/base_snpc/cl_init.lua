910f6e73d72f51da41e6068fe54aa3206c0911b4
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2023-04-02 17:18:34 +0300

    Update cl_init.lua

--- lua/entities/base_snpc/cl_init.lua
+++ lua/entities/base_snpc/cl_init.lua
@@ -1,4 +1,4 @@
--- incredible-gmod.ru
+-- from incredible-gmod.ru with <3
 
 include("shared.lua")
 
@@ -6,8 +6,18 @@ function ENT:Draw(f)
 	self:DrawModel(f)
 end
 
+local BASED = {} -- https://tenor.com/view/based-gigachad-chad-gif-23901955
+
+for class, sent in pairs(scripted_ents.GetList()) do
+	if sent.Base == "base_snpc" then
+		BASED[class] = true
+	end
+end
+
 function ENT:Initialize()
-	net.Receive(self.NetID or self.Folder, function()
+	BASED[self:GetClass()] = true
+
+	net.Receive(self.NetID or self:GetClass(), function()
 		local ent = net.ReadEntity()
 
 		if ent.OnUse then
@@ -15,3 +25,38 @@ function ENT:Initialize()
 		end
 	end)
 end
+
+local maxDist = 512 ^ 2
+local Ang90 = Angle(0, 0, 90)
+
+hook.Add("PreDrawEffects", "https://github.com/Be1zebub/GMod-Base-SNPC", function()
+	local ply = LocalPlayer()
+
+	for _, npc in ipairs(ents.GetAll()) do
+		if BASED[npc:GetClass()] and npc.Overhead then
+			local pos = npc:LookupBone("ValveBiped.Bip01_Head1")
+			if pos and pos > -1 then
+				pos = npc:GetBonePosition(pos) + npc:GetAngles():Up() * 12
+			else
+				pos = npc:GetPos() + npc:GetAngles():Up() * (npc:OBBMaxs().z + 3)
+			end
+
+			local dist = ply:GetPos():DistToSqr(pos)
+
+			if dist > (npc.Overhead.maxDist or maxDist) then continue end
+			local alpha = 1 - dist / (npc.Overhead.maxDist or maxDist)
+
+			local ang = ply:EyeAngles()
+			ang:RotateAroundAxis(ang:Forward(), 90)
+			ang:RotateAroundAxis(ang:Right(), 90)
+
+			Ang90.y = ang.y
+
+			cam.Start3D2D(pos, Ang90, 0.05)
+			surface.SetAlphaMultiplier(alpha)
+				npc.Overhead.draw(npc)
+			surface.SetAlphaMultiplier(1)
+			cam.End3D2D()
+		end
+	end
+end)
